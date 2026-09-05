cd0b3947a84fe4a68edb5c1c81de31ae50dfbc03
Asu Singh <enixes7@gmail.com>
2026-09-06 00:19:44 +0530

    Load creative portfolio styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import "./creative.css";
 
 const siteUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
   ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
