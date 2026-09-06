ad7ca324aabdca9acaa4c7b270662dffb2ec2de1
Asu Singh <enixes7@gmail.com>
2026-09-06 14:49:53 +0530

    Mount collision-aware doodle polish

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { WorkCvBoardPortal } from "@/components/WorkCvBoardPortal";
 import { ProfileLiveBoardPortal } from "@/components/ProfileLiveBoardPortal";
 import { ProfileLiveBoardAnimations } from "@/components/ProfileLiveBoardAnimations";
 import { BoardAliveLayer } from "@/components/BoardAliveLayer";
+import { BoardDoodlePolish } from "@/components/BoardDoodlePolish";
 import { BoardZoomBridge } from "@/components/BoardZoomBridge";
 
 export const metadata: Metadata = {
@@ -22,6 +23,7 @@ export default function Home() {
       <ProfileLiveBoardPortal />
       <WorkCvBoardPortal />
       <BoardAliveLayer />
+      <BoardDoodlePolish />
     </>
   );
 }
