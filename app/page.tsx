af40ff7d8e8abdd5f2ff4b034f2418b46ab921b6
Asu Singh <enixes7@gmail.com>
2026-09-12 15:51:33 +0530

    Mount continuous board spatial camera

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { HeroPreview } from "@/components/HeroPreview";
 import { WorkCvBoardPortal } from "@/components/WorkCvBoardPortal";
 import { BoardStoryAutoNavigator } from "@/components/BoardStoryAutoNavigator";
+import { BoardSpatialCamera } from "@/components/BoardSpatialCamera";
 import { ProfileLiveBoardPortal } from "@/components/ProfileLiveBoardPortal";
 import { ProfileLiveBoardAnimations } from "@/components/ProfileLiveBoardAnimations";
 import { BoardAliveLayer } from "@/components/BoardAliveLayer";
@@ -24,6 +25,7 @@ export default function Home() {
       <ProfileLiveBoardPortal />
       <WorkCvBoardPortal />
       <BoardStoryAutoNavigator />
+      <BoardSpatialCamera />
       <BoardAliveLayer />
       <BoardTechnicalSketches />
     </>
